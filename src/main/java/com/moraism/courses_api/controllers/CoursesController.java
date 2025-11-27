aa1e5f35a69370cd0644d856af3551c8ec5755cc
MaykonMorais <maykon.morais@protonmail.com>
2025-11-26 21:12:32 -0300

    feat: add environment configuration and Docker setup for database management

--- src/main/java/com/moraism/courses_api/controllers/CoursesController.java
+++ src/main/java/com/moraism/courses_api/controllers/CoursesController.java
@@ -1,10 +1,8 @@
 package com.moraism.courses_api.controllers;
 
 import com.moraism.courses_api.dto.CreateCourseRequestDTO;
-import com.moraism.courses_api.model.Course;
 import com.moraism.courses_api.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +16,6 @@ public class CoursesController {
     private CourseService courseService;
 
     @PostMapping
-    private ResponseEntity<Object> create(@RequestBody CreateCourseRequestDTO createCourseRequestDTO) {
-
+    private void create(@RequestBody CreateCourseRequestDTO createCourseRequestDTO) {
     }
 }
